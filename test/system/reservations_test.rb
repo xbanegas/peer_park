7001b401ffa218c2ae18f14f4d63ee90991d4309
JessKienz <jessica.kienzle@gmail.com>
2018-07-18 19:11:03 -0400

    added reservation scaffold and params

---
+++ test/system/reservations_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class ReservationsTest < ApplicationSystemTestCase
+  setup do
+    @reservation = reservations(:one)
+  end
+
+  test "visiting the index" do
+    visit reservations_url
+    assert_selector "h1", text: "Reservations"
+  end
+
+  test "creating a Reservation" do
+    visit reservations_url
+    click_on "New Reservation"
+
+    click_on "Create Reservation"
+
+    assert_text "Reservation was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Reservation" do
+    visit reservations_url
+    click_on "Edit", match: :first
+
+    click_on "Update Reservation"
+
+    assert_text "Reservation was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Reservation" do
+    visit reservations_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Reservation was successfully destroyed"
+  end
+end
