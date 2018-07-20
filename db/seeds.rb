6925eced1b7085052cc2967a701c407998cb4674
JessKienz <jessica.kienzle@gmail.com>
2018-07-19 20:09:13 -0400

    enhancement-24-jess-add-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Faker::Config.locale = 'en-US'
+
+100.times do |num|
+  u = User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8),
+    encrypted_password: Faker::Internet.password(8),
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    cell_number: Faker::PhoneNumber.cell_phone,
+    vehicle_make: Faker::Vehicle.make,
+    vehicle_model: Faker::Vehicle.model,
+    vehicle_color: Faker::Vehicle.color,
+  )
+  Spot.create(
+    user_id: u.id,
+    address_1: Faker::Address.street_address,
+    address_2: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip: Faker::Address.zip
+  )
+end
