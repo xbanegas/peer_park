6df25ca099aa3826b6b7985e56d1605bd48e54fb
RafaMustafa3 <rafi869@gmail.com>
2018-07-21 19:10:57 -0400

    added state validation

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,16 @@
 class Space < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  validates :state, :presence => true
+
+
+  def has_valid_state
+    states = ["AL", "AR", "AZ", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    unless states.include?(self.state)
+      self.state = ""
+      return false
+    end
+    return true
+  end
+
 end
