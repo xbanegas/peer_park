8d063b397b58f17c26ef877aafba9e6fe9838b1e
xbanegas <xbanegas@gmail.com>
2018-07-22 16:17:23 -0400

    configure routing after signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    # puts params
-    '/spaces/new' # Or :prefix_to_your_route
+		if CGI.parse(URI.parse(request.headers["Referer"]).query)["list"][0] == "true"
+			new_space_path
+		else
+			new_vehicle_path
+		end
   end
 end
