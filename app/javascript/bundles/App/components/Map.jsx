ff40ecea469feaccac38ea8ea27ea2ebe31b0d65
xbanegas <xbanegas@gmail.com>
2018-07-22 23:37:59 -0400

    markers present but not visible

--- app/javascript/bundles/App/components/Map.jsx
+++ app/javascript/bundles/App/components/Map.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {initMap, loadPosition, addVendorsToMap} from '../utils/mapUtils';
+import {initMap, loadPosition, addSpacesToMap} from '../utils/mapUtils';
 
 export default class Map extends React.Component {
   // static propTypes = { name: PropTypes.string.isRequired };
@@ -12,18 +12,28 @@ export default class Map extends React.Component {
 			top: 0,
 			bottom: 0,
       width: '75%',
-      height: 400,
+      height: '80vh',
       'margin-left': 'auto',
       'margin-right': 'auto'
-		};
+    };
+    this.state={map:{}, userLoc:null}
   }
 
   async componentDidMount() {
     let userLoc = await loadPosition();
     let map = await initMap(userLoc, this.mapContainer, this.props.MAPBOX_API_KEY)
+    map = addSpacesToMap(document, map, userLoc, this.handleReserveClick);
+    let state = {...this.state}
+		state.userLoc = userLoc;
+		state.map = map;
+		this.setState(state);
   }
 
-
+	async handleReserveClick(destLoc){
+		// let routeData = await this.getRoute(destLoc)
+		// return routeData;
+  }
+  
 	render() {
 		console.log(this.props);
     return (
