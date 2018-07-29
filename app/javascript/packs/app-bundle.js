48417866e8d48de63f7e5fc66fcfbd204c804b65
xbanegas <xbanegas@gmail.com>
2018-07-28 20:53:35 -0400

    working stripe amount

--- app/javascript/packs/app-bundle.js
+++ app/javascript/packs/app-bundle.js
@@ -2,8 +2,10 @@ import ReactOnRails from 'react-on-rails';
 import "babel-polyfill";
 import Map from '../bundles/App/components/Map.jsx';
 import Calendar from '../bundles/App/components/Calendar.jsx';
+import Reservation from '../bundles/App/components/Reservation.jsx';
 
 ReactOnRails.register({
   Map,
-  Calendar
+  Calendar,
+  Reservation
 });
