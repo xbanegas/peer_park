079ef430b54745e1f4910717835fd4bd7df9582d
xbanegas <xbanegas@gmail.com>
2018-07-28 23:49:55 -0400

    working twilio

--- app/workers/expired_reservation_worker.rb
+++ app/workers/expired_reservation_worker.rb
@@ -5,11 +5,13 @@ class ExpiredReservationWorker
   twilio_token=ENV["TWILIO_API_TOKEN"]
   twilio_secret=ENV["TWILIO_SECRET"]
 
-  @client = Twilio::REST::Client.new twilio_token, twilio_secret, twilio_sid 
-  puts @client
   def perform(*args)
-    @client.messages.create(
-      from: '+14159341234',
+    twilio_sid=ENV["TWILIO_API_SID"]
+    twilio_token=ENV["TWILIO_API_TOKEN"]
+    twilio_secret=ENV["TWILIO_SECRET"]
+    @client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @client.api.messages.create(
+      from: '+19548704155',
       to: '+13057535083',
       body: 'Hey there!'
     ) 
