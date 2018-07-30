22a5756de7e68c15026ab96e0f35c8b525c84de8
xbanegas <xbanegas@gmail.com>
2018-07-30 17:26:23 -0400

    lister twilio message

--- app/workers/new_reservation_worker.rb
+++ app/workers/new_reservation_worker.rb
@@ -7,14 +7,27 @@ class NewReservationWorker
     @client = Twilio::REST::Client.new twilio_sid, twilio_token
     
     parker = reservation.vehicle.user
+    start_time =  reservation.start_time.strftime("%I:%M%p") + reservation.start_time.strftime(" on %m/%d/%Y") 
     unless parker.cell_number.nil?
-      start_time =  reservation.start_time.strftime("%I:%M%p") + reservation.start_time.strftime(" on %m/%d/%Y") 
       parker_message = "You've reserved a space at #{reservation.space.make_address} for #{reservation.vehicle.license_plate}"\
-        " starting at #{start_time} for #{reservation.duration} hour. "
+        " starting at #{start_time} for #{reservation.duration} hours. "
+
       @client.api.messages.create(
         from: '+19548704155',
         to: '+1'+parker.cell_number,
         body: parker_message
+      ) 
+    end
+
+    lister = reservation.space.user
+    unless lister.cell_number.nil?
+      lister_message = "Your space at #{reservation.space.make_address} was reserved by #{reservation.vehicle.license_plate}" \
+      " starting at #{start_time} for #{reservation.duration} hours."
+
+      @client.api.messages.create(
+        from: '+19548704155',
+        to: '+1'+lister.cell_number,
+        body: lister_message 
       ) 
     end
   end
